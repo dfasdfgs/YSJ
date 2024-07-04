9b53b9d9b27cff4d6d45d628f2c74a49bf8f7b8c
leemj0112 <163072105+leemj0112@users.noreply.github.com>
2024-07-04 16:18:49 +0900

    디버깅 중
    코루틴 오류로 추정 중
    (재시작 시 코루틴은 재실행이 안됨)

--- Assets/Scripts/FadeScene.cs
+++ Assets/Scripts/FadeScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FadeScene : MonoBehaviour
@@ -10,10 +11,19 @@ public class FadeScene : MonoBehaviour
     public GameObject FadeObject;
 
     // Start is called before the first frame update
+
+    
+
     void Start()
     {
         FadeImage.gameObject.SetActive(true);
+        FadeObject.SetActive(true);
         StartCoroutine(Fade());
+
+        if (FadeObject == true)
+        {
+            Debug.Log("ƒ—¡¸"); 
+        }
     }
 
     private IEnumerator Fade()
@@ -21,15 +31,15 @@ public class FadeScene : MonoBehaviour
         while (FadeAlpha >= 0)
         {
             FadeAlpha -= 0.01f;
-            Debug.Log("≈∏¿Ã∏” Ω√¿€");
+            //Debug.Log("≈∏¿Ã∏” Ω√¿€");
 
             FadeImage.color = new Color(FadeImage.color.r, FadeImage.color.g, FadeImage.color.b, FadeAlpha);
             FadeText.color = new Color(FadeText.color.r, FadeText.color.g, FadeText.color.b, FadeAlpha);
 
             yield return new WaitForSeconds(0.02f);
            
-
         } 
-         FadeObject.SetActive(false); 
+         FadeObject.SetActive(false);
+        Debug.Log("≤®¡¸");
     }
 }
